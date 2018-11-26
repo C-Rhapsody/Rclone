7f9fde3aace1134c2f4af0012280c848c4764242
C-Rhapsody <43021175+C-Rhapsody@users.noreply.github.com>
2018-11-26 14:40:12 +0900

    Personally Rclone Mount Script Example
    FuseType보다는 디렉토리 또는 파일을 체크하는게 확실해서 간단하게 :)

---
+++ MountRclone.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#
+# Environment 
+# OS : Ubuntu 18.04 LTS
+# Rclone Ver : v1.44
+#
+# Cron Use Exp
+#   */5 * * * * /bin/bash /<path>/MountRclone.sh
+
+MountDir = "/home/<username>/rclone/cache/"
+RcloneConf = "/home/<username>/.config/rclone/rclone.conf"
+
+if [ ! -d "$MountDir/Temp" ]; then
+    rclone mount cache: "$MountDir" \
+    --config "RcloneConf" \
+    --allow-non-empty --allow-other --read-only \
+    --uid <current_uid> --gid <current_gid> &
+else
+    echo "Pre Mounting"
+fi
